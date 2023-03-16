d86a73a966ced70796f44db8809f76ce7075f0a7
fakE81 <62438392+fakE81@users.noreply.github.com>
2023-03-16 21:49:26 +0200

    Prefabs and models

---
+++ Assets/Scripts/UI/DamagePop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = new Vector3(0f, 1f, 0f);
+    }
+}
