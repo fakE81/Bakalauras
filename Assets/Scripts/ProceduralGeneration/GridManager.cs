9b2fc82f6fd4824a96f5b0a9bc5943c455b0addf
fakE81 <62438392+fakE81@users.noreply.github.com>
2023-04-20 19:55:06 +0300

    Refactoring

--- Assets/Scripts/ProceduralGeneration/GridManager.cs
+++ Assets/Scripts/ProceduralGeneration/GridManager.cs
@@ -13,7 +13,7 @@ public class GridManager : MonoBehaviour
 
     public GridCellObject[] pathCellsOjbects;
     public GridCellObject[] sceneryCellObjects;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,15 +28,16 @@ public class GridManager : MonoBehaviour
         for (int i = 0; i < blocksSize; i++)
         {
             pathGenerator = new PathGenerator(temporaryGridWidth, gridHeight);
-        
+
             List<Vector2Int> pathCells = pathGenerator.GeneratePath(startingPos);
             int pathSize = pathCells.Count;
             // For galima cia butu ideti
-            while (pathSize < minPathLength || !pathCells.Contains(new Vector2Int(endCellX,4)))
+            while (pathSize < minPathLength || !pathCells.Contains(new Vector2Int(endCellX, 4)))
             {
                 pathCells = pathGenerator.GeneratePath(startingPos);
                 pathSize = pathCells.Count;
             }
+
             yield return StartCoroutine(CreateGrid(pathCells, startingPos, temporaryGridWidth));
             startingPos += 9;
             endCellX = startingPos + 8;
@@ -49,18 +50,20 @@ public class GridManager : MonoBehaviour
         yield return StartCoroutine(LayPathCells(pathCells));
         yield return StartCoroutine(LaySceneryCells(endCellX, temporaryGridWidth));
     }
-    
+
     private IEnumerator LayPathCells(List<Vector2Int> pathCells)
     {
         foreach (var pathCell in pathCells)
         {
             int neigbourValue = pathGenerator.GetCellNeighbourValue(pathCell.x, pathCell.y);
-            Debug.Log("Tile " + pathCell.x + "," + pathCell.y + " NValue" +neigbourValue);
+            Debug.Log("Tile " + pathCell.x + "," + pathCell.y + " NValue" + neigbourValue);
             GameObject pathTile = pathCellsOjbects[neigbourValue].cellPrefab;
-            GameObject pathTileCell = Instantiate(pathTile, new Vector3(pathCell.x, 0f, pathCell.y), Quaternion.identity);
+            GameObject pathTileCell =
+                Instantiate(pathTile, new Vector3(pathCell.x, 0f, pathCell.y), Quaternion.identity);
             pathTileCell.transform.Rotate(0f, pathCellsOjbects[neigbourValue].yRotation, 0f);
             yield return new WaitForSeconds(0.1f);
         }
+
         yield return null;
     }
 
@@ -73,11 +76,13 @@ public class GridManager : MonoBehaviour
                 if (pathGenerator.CellIsFree(x, y))
                 {
                     int randomSceneryCellIndex = Random.Range(0, sceneryCellObjects.Length);
-                    Instantiate(sceneryCellObjects[randomSceneryCellIndex].cellPrefab, new Vector3(x, 0f, y), Quaternion.identity);
+                    Instantiate(sceneryCellObjects[randomSceneryCellIndex].cellPrefab, new Vector3(x, 0f, y),
+                        Quaternion.identity);
                     yield return new WaitForSeconds(0.05f);
                 }
             }
         }
+
         yield return null;
     }
 }
